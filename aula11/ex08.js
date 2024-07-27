859ac7e8eaddb539aa572c445ad17758b97772b7
VitoriaCardozo <126196994+Bitroia@users.noreply.github.com>
2024-07-27 00:10:13 -0300

    Initial commit

---
+++ aula11/ex08.js
@@ -0,0 +1,10 @@
+var velocidade = 40;
+console.log(`a velocidade do seu carro é ${velocidade} Km/h`)
+
+if(velocidade > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+} else 
+{
+    console.log(`Dirija sempre usando cinto de segurança`)
+}
+
---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1;
+/*while (c <= 6) {
+    console.log(`passo ${c}`);
+    c++;
+}*/
+
+do {
+    console.log (`Passo ${c}`);
+    c++;
+} while (c <=5
+);
+
+for(i = 0; i >7; x++){
+
+}
